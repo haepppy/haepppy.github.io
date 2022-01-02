0e23f1b44dd7e086ce23ede0b69ca5db94fbf883
haepppy <godls2213@naver.com>
2022-01-02 16:46:27 +0900

    .

--- assets/js/project1/clock.js
+++ assets/js/project1/clock.js
@@ -7,8 +7,8 @@ function getClock() {
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
     const year = date.getFullYear();
-    const month = String(date.getMonth()).padStart(2,"0");
-    const day = String(date.getDay()).padStart(2,"0");
+    const month = String(date.getMonth()+1).padStart(2,"0");
+    const day = String(date.getDate()).padStart(2,"0");
     const week = ['일', '월', '화', '수', '목', '금', '토'];
     const weekName = week[date.getDay()];
 
